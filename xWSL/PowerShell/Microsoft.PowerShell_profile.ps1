54e7c60c24be98e3d6bf32f4b72c754c0261ca5c
vimi <vimi.vimi@dell>
2023-02-09 15:19:50 +0100

    add

--- xWSL/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-Import-Module -Name Terminal-Icons
-
-Set-Alias -Name rec -Value "C:\Users\$env:UserName\OneDrive\Linux\Skripty\stream_record_win.ps1"
-
-$ENV:STARSHIP_CONFIG = "$HOME\Documents\PowerShell\starship.toml"
-$ENV:STARSHIP_DISTRO = "者 "
-Invoke-Expression (&starship init powershell)
