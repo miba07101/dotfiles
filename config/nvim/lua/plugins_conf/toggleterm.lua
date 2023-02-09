2be9b2ea264ddd9448d5ef9f5fe48e4185e3fb7f
vimi <vimi.vimi@dell>
2023-02-09 19:09:35 +0100

    toggleterm lazygit

--- config/nvim/lua/plugins_conf/toggleterm.lua
+++ config/nvim/lua/plugins_conf/toggleterm.lua
@@ -36,3 +36,29 @@ local web = Terminal:new({ direction = "horizontal", cmd = "live-server ." })
 function _WEB_TOGGLE()
 	web:toggle()
 end
+
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd("startinsert!")
+      end,
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+
