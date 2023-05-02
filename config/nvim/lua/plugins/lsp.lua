8b4a99042b3983e2ff9f2183ad640f45f32ba3eb
vimi <vimi.vimi@dell>
2023-05-02 12:08:57 +0200

    up

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,17 @@ local servers = {
   cssls = {},
   html = {},
   jsonls = {},
+  emmet_ls = {
+               filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'javascript', 'typescript', 'markdown' },
+               init_options = {
+                  html = {
+                     options = {
+                        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L26
+                        ["bem.enabled"] = true,
+                     },
+                  },
+               }
+            },
   lua_ls = {
     settings = {
       Lua = {
