348b53a8250d63597aa9b549bdf14246bf1431a6
miba07101 <vimi.vimi@dell>
2024-04-22 20:53:03 +0200

    up ueberzug try

--- config/nvim/lua/plugin-manager.lua
+++ config/nvim/lua/plugin-manager.lua
@@ -549,6 +549,22 @@ require("lazy").setup({
   },
 },
 
+-- {
+--     "vhyrro/luarocks.nvim",
+--     priority = 1001, -- this plugin needs to run before anything else
+--     opts = {
+--         rocks = { "magick" },
+--     },
+-- },
+-- {
+--     "3rd/image.nvim",
+--     dependencies = { "luarocks.nvim" },
+--     config = function()
+--       require("image").setup({
+--         backend = "ueberzug",
+--       })
+--     end
+-- },
 
   -- keybidings whichkey
   { "folke/which-key.nvim",
