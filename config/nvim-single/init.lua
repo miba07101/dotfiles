bef9c1fd0c2d2ae6361bb17d1edfc923d15feff0
vimi <milan.balaz@outlook.com>
2024-12-11 13:52:11 +0100

    Update .dotfiles nvim-single 2

--- config/nvim-single/init.lua
+++ config/nvim-single/init.lua
@@ -29,6 +29,107 @@ local os_type = detect_os_type()
 print("detected os: " .. detect_os_type())
 
 -- +--------------------------------------------------------------------------+
+-- OPTIONS
+-- +--------------------------------------------------------------------------+
+
+local function options()
+  local opt = vim.opt
+
+  -- file
+  opt.backup = false -- create a backup file
+  opt.clipboard = "unnamedplus" -- system clipboard
+  opt.concealcursor = "" -- conceal cursor disable
+  opt.conceallevel = 0 -- conceal level disable
+  opt.fileencoding = "utf-8" -- character encoding
+  opt.filetype = "plugin" -- plugin loading of file types
+  opt.hidden = true -- switching from unsaved buffers
+  opt.inccommand = 'split' -- preview substitutions live, as you type
+  opt.iskeyword:remove("_") -- oddeli slova pri mazani, nebude brat ako jedno slovo
+  opt.matchtime = 2 -- duration of showmatch, default 5
+  opt.mouse = "a" -- mouse
+  opt.scrolloff = 5 -- how many lines are displayed on the upper and lower sides of the cursor
+  opt.showmode = true -- display the current vim mode (no need)
+  opt.sidescrolloff = 8 -- number of columns to keep at the sides of the cursor
+  opt.splitbelow = true -- splitting window below
+  opt.splitright = true -- splitting window right
+  opt.swapfile = false -- create a swap file
+  opt.syntax = "on"
+  opt.termguicolors = true -- terminal supports more colors
+  opt.timeoutlen = 300 -- time to wait for a mapped sequence to complete, default 1000
+  opt.updatetime = 100 -- speed up response time
+  opt.whichwrap:append("<,>,[,],h,l") -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+  opt.wrap = false -- disable wrapping of lines longer than the width of window
+  opt.writebackup = false -- create backups when writing files
+
+  -- completition
+  opt.completeopt = { "menuone" , "noselect" } -- completion options
+  opt.pumheight = 10 -- completion menu height
+  opt.wildmenu = true  -- make tab completion for files/buffers act like bash
+
+  -- fold
+  opt.foldenable = true -- folding allowed
+  opt.foldlevel = 1 -- folding from lvl 1
+  opt.foldmethod = "expr" -- folding method
+  -- opt.foldexpr = "nvim_treesitter#foldexpr()" -- folding method use treesitter
+  opt.foldcolumn = "1" -- folding column show
+  -- opt.foldtext=[[getline(v:foldstart)]]        -- Folding - disable all chunk when folded
+  opt.fillchars:append({
+      eob = " ",
+      fold = " ",
+      foldopen = "",
+      foldsep = " ",
+      foldclose = ""
+      })
+
+  -- indention
+  local indent = 2
+  opt.autoindent = true -- auto indentation
+  opt.expandtab = true -- convert tabs to spaces (prefered for python)
+  opt.shiftround = true -- use multiple of shiftwidth when indenting with "<" and ">"
+  opt.shiftwidth = indent  -- spaces inserted for each indentation
+  opt.smartindent = true -- make indenting smarter
+  opt.softtabstop = indent -- when hitting <BS>, pretend like a tab is removed, even if spaces
+  opt.tabstop = indent -- insert spaces for a tab
+
+  -- search
+  opt.hlsearch = true -- search highlighting
+  opt.ignorecase = true -- ignore case when searching
+  opt.incsearch = true -- highlight while searching
+  opt.smartcase = true -- intelligent case sensitivity when searching (if there is upper case, turn off case ignoring)
+  opt.wildignore = opt.wildignore + { "*/node_modules/*", "*/.git/*", "*/vendor/*" }
+  opt.wrapscan = true -- search the entire file repeatedly
+
+  -- ui
+  --opt.cmdheight = 0 -- command line height
+  opt.cursorline = true -- highlight the current line
+  --opt.laststatus = 3 -- global status bar (sposobuje nefunkcnost resource lua.init)
+  opt.number = true -- absolute line numbers
+  opt.numberwidth = 1 -- number column width, default is 4
+  --opt.relativenumber = true -- relative line numbers
+  opt.signcolumn = "yes" -- symbol column width
+  opt.list = true -- show some invisible characters (tabs...
+  opt.listchars = { eol = "¬", tab = "› ", trail = "·" } -- list characters
+
+  -- nastavenia kurzoru
+  if os_type == "wsl" then
+    opt.guicursor = {"n-v-c:block,i-ci-ve:bar-blinkwait200-blinkoff150-blinkon150"}  -- Cursor help: guicursor
+    opt.shell = "/bin/bash"
+  elseif os_type == "linux" then
+    opt.shell = "/bin/zsh"
+  elseif os_type == "windows" then
+    opt.shell = "pwsh.exe"
+    opt.shellcmdflag = "-NoLogo"
+    opt.shellquote = ""
+    opt.shellxquote = ""
+  else
+    opt.shell = "/bin/zsh"
+  end
+
+end
+
+options()
+
+-- +--------------------------------------------------------------------------+
 -- KEYMAPS
 -- +--------------------------------------------------------------------------+
 
@@ -57,104 +158,24 @@ end
 keymaps()
 
 -- +--------------------------------------------------------------------------+
--- OPTIONS
+-- AUTOCOMMANDS
 -- +--------------------------------------------------------------------------+
-
-local function options()
-  local opt = vim.opt
-
-  -- search
-  opt.hlsearch = true                          -- Allow search highlighting
-  opt.ignorecase = true                        -- Ignore case when searching
-  opt.incsearch = true                         -- Highlight while searching
-  opt.smartcase = true                         -- Intelligent case sensitivity when searching (if there is upper case, turn off case ignoring)
-  opt.wildignore = opt.wildignore + { "*/node_modules/*", "*/.git/*", "*/vendor/*" }
-  opt.wildmenu = true   -- make tab completion for files/buffers act like bash
-
-  -- indention
-  local indent = 2
-  opt.autoindent = true -- auto indentation
-  opt.smartindent = true -- make indenting smarter
-  opt.shiftround = true    -- use multiple of shiftwidth when indenting with "<" and ">"
-  opt.expandtab = true -- convert tabs to spaces (prefered for python)
-  opt.shiftwidth = indent  -- spaces inserted for each indentation
-  opt.tabstop = indent -- insert spaces for a tab
-  opt.softtabstop = indent -- when hitting <BS>, pretend like a tab is removed, even if spaces
-  opt.list = true
-  opt.listchars = { eol = "¬", tab = "› " }
-
-  -- file
-  opt.backup = false                           -- Whether to create a backup file
-  opt.clipboard = "unnamedplus" -- Use the system clipboard
-  opt.mouse = "a" -- Allow the use of the mouse
-  opt.fileencoding = "utf-8"                   -- Set the character encoding of the file where the current buffer is located
-  opt.filetype = "plugin"                      -- Allow plugin loading of file types
-  --opt.hidden = true                            -- Allow switching from unsaved buffers
-
-  -- ui
-  --opt.cmdheight = 0                            -- Set command line height
-  opt.number = true -- Allow absolute line numbers
-  --opt.relativenumber = true                    -- Allow relative line numbers
-  opt.cursorline = true                        -- Highlight the current line
-  opt.laststatus = 3                           -- Enable global status bar
-
-  -- completition
-  opt.completeopt = { "menuone" , "noselect" } -- Set completion options
-
-  -- fold
-  opt.foldenable = true                        -- Folding allowed
-  opt.foldlevel = 1                            -- Folding from lvl 1
-  opt.foldmethod = "expr"                      -- Folding method
-  opt.foldexpr = "nvim_treesitter#foldexpr()"  -- Folding method use treesitter
-  -- opt.foldcolumn = "1"                      -- Folding column show
-  opt.foldtext=[[getline(v:foldstart)]]        -- Folding - disable all chunk when folded
-  opt.fillchars:append({
-      eob = " ",
-      fold = " ",
-      foldopen = "",
-      foldsep = " ",
-      foldclose = ""
+local function autocommands()
+  -- [[highlight on yank]]
+  vim.api.nvim_create_autocmd("TextYankPost", {
+      callback = function()
+      vim.highlight.on_yank({
+          higroup = "incsearch",
+          timeout = 300,
+          })
+      end,
+      group = mygroup,
+      desc = "Highlight yanked text",
       })
-
-opt.conceallevel = 0                         -- Conceal level disable
-opt.concealcursor = ""                       -- Conceal cursor disable
-opt.iskeyword:remove("_")                       -- Oddeli slova pri mazani, nebude brat ako jedno slovo
-opt.matchtime = 2                            -- Default is 5, set the duration of showmatch
-opt.numberwidth = 1                          -- Set the width of the number column, default is 4
-opt.pumheight = 10                           -- Set the height of the completion menu
-opt.scrolloff = 5                            -- Set how many lines are always displayed on the upper and lower sides of the cursor
-opt.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,globals"    -- Set options for saving sessions
-opt.showmode = false                         -- Allows to display the current vim mode (no need)
-opt.sidescrolloff = 8                        -- Number of columns to keep at the sides of the cursor
-opt.signcolumn = "yes:1"                     -- Set the width of the symbol column, if not set, it will cause an exception when displaying the icon
-opt.splitbelow = true                        -- Splitting a new window below the current one
-opt.splitright = true                        -- Splitting a new window at the right of the current one
-opt.swapfile = false                         -- Whether to create a swap file
-opt.syntax = "on"
-opt.termguicolors = true                     -- Terminal supports more colors
-opt.timeoutlen = 300                         -- Set timeout
-opt.updatetime = 100                         -- Speed up response time
--- opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the previous/next line when the beginning/end of line is reached
-opt.wrap = false                             -- Disable wrapping of lines longer than the width of window
-opt.wrapscan = true                          -- Allows to search the entire file repeatedly (continuation of the search after the last result will return to the first result)
-opt.writebackup = false                      -- Whether to create backups when writing files
-
-  -- nastavenia kurzoru
-  if os_type == "wsl" then
-    opt.guicursor = {"n-v-c:block,i-ci-ve:bar-blinkwait200-blinkoff150-blinkon150"}  -- Cursor help: guicursor
-    opt.shell = "/bin/bash"
-  elseif os_type == "linux" then
-    opt.shell = "/bin/zsh"
-  elseif os_type == "windows" then
-    opt.shell = "pwsh.exe"
-    opt.shellcmdflag = "-NoLogo"
-    opt.shellquote = ""
-    opt.shellxquote = ""
-  else
-    opt.shell = "/bin/zsh"
-  end
-
 end
 
-options()
+autocommands()
 
+-- +--------------------------------------------------------------------------+
+-- LAZY MANAGER
+-- +--------------------------------------------------------------------------+
