bc38b7525eabcc5332b4e199f8491ac5531215ea
miba07101 <vimi.vimi@dell>
2025-01-10 18:52:57 +0100

    Update .dotfiles nvim

--- config/nvim-single/init.lua
+++ config/nvim-single/init.lua
@@ -932,16 +932,16 @@ require("lazy").setup(
           -- require("codeium").setup({})
 
           cmp.setup({
-            -- enabled = function()
-            --   -- disable completion in comments
-            --   if require"cmp.config.context".in_treesitter_capture("comment")==true
-            -- or require"cmp.config.context".in_syntax_group("Comment") then
-            --     return false
-            --   else
-            --     return true
-            --   end
-            -- end,
-            enabled = true,
+            enabled = function()
+              -- disable completion in comments
+              if require"cmp.config.context".in_treesitter_capture("comment")==true
+            or require"cmp.config.context".in_syntax_group("Comment") then
+                return false
+              else
+                return true
+              end
+            end,
+            -- enabled = true,
             snippet = {
               expand = function(args)
                 -- for luasnip
