8957e3aee9f71dccd939f181d1fc7f9fd3dca842
miba07101 <vimi.vimi@dell>
2025-02-03 16:47:06 +0100

    up dotfiles vimi (2025-02-03 16:47:25)

--- config/nvim-single/init.lua
+++ config/nvim-single/init.lua
@@ -1722,39 +1722,39 @@ require("lazy").setup(
           -- vim.g.molten_virt_lines_off_by_1 = true -- this will make it so the output shows up below the \`\`\` cell delimiter
           -- vim.g.molten_virt_text_output = true
 
-          -- change the configuration when editing a python file{{{
-          vim.api.nvim_create_autocmd("BufEnter", {
-            pattern = "*.py",
-            callback = function(e)
-              if string.match(e.file, ".otter.") then
-                return
-              end
-              if require("molten.status").initialized() == "Molten" then -- this is kinda a hack...
-                vim.fn.MoltenUpdateOption("virt_lines_off_by_1", false)
-                vim.fn.MoltenUpdateOption("virt_text_output", false)
-              else
-                vim.g.molten_virt_lines_off_by_1 = false
-                vim.g.molten_virt_text_output = false
-              end
-            end,
-          })-- }}}
+          -- -- change the configuration when editing a python file{{{
+          -- vim.api.nvim_create_autocmd("BufEnter", {
+          --   pattern = "*.py",
+          --   callback = function(e)
+          --     if string.match(e.file, ".otter.") then
+          --       return
+          --     end
+          --     if require("molten.status").initialized() == "Molten" then -- this is kinda a hack...
+          --       vim.fn.MoltenUpdateOption("virt_lines_off_by_1", false)
+          --       vim.fn.MoltenUpdateOption("virt_text_output", false)
+          --     else
+          --       vim.g.molten_virt_lines_off_by_1 = false
+          --       vim.g.molten_virt_text_output = false
+          --     end
+          --   end,
+          -- })-- }}}
 
-          -- Undo those config changes when we go back to a markdown or quarto file{{{
-          vim.api.nvim_create_autocmd("BufEnter", {
-            pattern = { "*.qmd", "*.md", "*.ipynb" },
-            callback = function(e)
-              if string.match(e.file, ".otter.") then
-                return
-              end
-              if require("molten.status").initialized() == "Molten" then
-                vim.fn.MoltenUpdateOption("virt_lines_off_by_1", true)
-                vim.fn.MoltenUpdateOption("virt_text_output", true)
-              else
-                vim.g.molten_virt_lines_off_by_1 = true
-                vim.g.molten_virt_text_output = true
-              end
-            end,
-          })-- }}}
+          -- -- Undo those config changes when we go back to a markdown or quarto file{{{
+          -- vim.api.nvim_create_autocmd("BufEnter", {
+          --   pattern = { "*.qmd", "*.md", "*.ipynb" },
+          --   callback = function(e)
+          --     if string.match(e.file, ".otter.") then
+          --       return
+          --     end
+          --     if require("molten.status").initialized() == "Molten" then
+          --       vim.fn.MoltenUpdateOption("virt_lines_off_by_1", true)
+          --       vim.fn.MoltenUpdateOption("virt_text_output", true)
+          --     else
+          --       vim.g.molten_virt_lines_off_by_1 = true
+          --       vim.g.molten_virt_text_output = true
+          --     end
+          --   end,
+          -- })-- }}}
 
         end,-- }}}
         keys = {-- {{{
