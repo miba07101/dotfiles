e825d5d3a304a04ee27fb1a5b31710c7a25153e3
miba07101 <vimi.vimi@dell>
2025-03-30 18:12:57 +0200

    up vimi-jonsbo xWindows/PowerShell/profile.ps1 2025-03-30 18:12

--- xWindows/PowerShell/profile.ps1
+++ xWindows/PowerShell/profile.ps1
@@ -74,6 +74,35 @@ function ytd {
   . "$scriptPath\youtube-download.ps1"
     YoutubeDownload
 }
+
+function fireup {
+    $firefoxDir = Get-ChildItem "$env:APPDATA\Mozilla\Firefox\Profiles" -Directory |
+        Where-Object { $_.Name -like "*default-release*" } |
+        Select-Object -First 1 -ExpandProperty FullName
+    
+    if (-not $firefoxDir) {
+        Write-Host "Firefox profile directory not found."
+        exit 1
+    }
+    
+    Write-Host "Full Firefox path: $firefoxDir"
+    
+    $userJsPath = Join-Path $firefoxDir "user.js"
+    $prefsJsPath = Join-Path $firefoxDir "prefs.js"
+    
+    if (-not (Test-Path $userJsPath)) {
+        New-Item -Path $userJsPath -ItemType File | Out-Null
+    }
+    
+    # Remove lines containing "browser.newtabpage.pinned"
+    (Get-Content $userJsPath) | Where-Object { $_ -notmatch "browser\.newtabpage\.pinned" } | Set-Content $userJsPath
+    
+    # Append matching lines from prefs.js to user.js
+    Get-Content $prefsJsPath | Where-Object { $_ -match "browser\.newtabpage\.pinned" } | Add-Content $userJsPath
+    
+    Write-Host "Firefox prefs.js and user.js updated"
+}
+
 # -----------------------------------------------------------------------------
 # environment variables
 # -----------------------------------------------------------------------------
