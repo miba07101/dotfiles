a8a20ef464fadaf7a2c02578210a89147b3f6dae
miba07101 <vimi.vimi@dell>
2025-05-04 13:08:27 +0200

    up vimi-jonsbo config/nvim-single/init.lua 2025-05-04 13:08

--- config/nvim-single/init.lua
+++ config/nvim-single/init.lua
@@ -1549,6 +1549,17 @@ require("lazy").setup({
         dashboard = {-- {{{
           enabled = true,
           preset = {
+            keys = {
+              { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+              { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+              { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+              { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+              { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+              -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+              { icon = " ", key = "p", desc = "Projects", action = ":lua Snacks.dashboard.pick('projects', {dev = '~/git-repos/', recent = false})" },
+              { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+              { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            },
             header = [[
 ██╗   ██╗██╗███╗   ███╗██╗██╗   ██╗██╗███╗   ███╗
 ██║   ██║██║████╗ ████║██║██║   ██║██║████╗ ████║
@@ -1560,8 +1571,18 @@ require("lazy").setup({
           },
           sections = {
             { section = "header" },
+            -- { section = "terminal", cmd = "echo 'hello'", random = 2 },
             { section = "keys", gap = 1, padding = 1 },
-            {icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1,},
+            {
+              -- icon = " ",
+              title = "Projects",
+              section = "projects",
+              -- limit=1,
+              -- dirs = "~/git-repos/python/",
+              -- pick=true,
+              -- indent = 2,
+              -- padding = 1,
+            },
             { section = "startup" },
           },
         },-- }}}
